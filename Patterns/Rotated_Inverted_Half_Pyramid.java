c3c8e4cc8f2a6f644bd10f589ab1cf7216fb2127
ShreyaKumar-dev <shreyatiwary2003@gmail.com>
2024-09-11 09:55:28 +0530

    Advance Patterns

--- Patterns/Rotated_Inverted_Half_Pyramid.java
+++ Patterns/Rotated_Inverted_Half_Pyramid.java
@@ -3,13 +3,14 @@
 public class Rotated_Inverted_Half_Pyramid {
     public static void main(String[] args){
         int n = 4;
+        
         for(int i=n; i>=1; i--){
-            for(int j=1; i<=n; j++){
-                if(j<=n-i){
-                    System.out.print(" ");
-                } else{
-                    System.out.print("*");
-                }
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
             }
             System.out.println();
         }
